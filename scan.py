5e58ddc2ed3ed1c66df9320eb30aba7934770220
LilTerror11 <144447975+LilTerror11@users.noreply.github.com>
2024-09-05 17:10:51 +0930

    FIXY MIC FIXY FAce

--- scan.py
+++ scan.py
@@ -1,32 +1,49 @@
 import os
+from typing import Any
 
-path = {}
+Assets = {}
 
-
-def scan_dir(directory: str, log: bool = False):
+def scan_dir(directory: str, loops=0):
     scanned = os.scandir(directory)
-    for path in scanned:
+    help_me = []
+    scanny = scanned
+    for x in scanny:
+        #print(x)
+        help_me.append(x)
+    #print(help_me)
+
+    #print(scanned)
+
+    for path in help_me:
+        i = help_me.index(path) + 1
         path: os.DirEntry
         if path.is_dir():
             try:
-                dict_path = str(f"{directory}/{path.name}").split("/")
-                dict_path = '/'.join(dict_path)
+                print("  "*loops + "\"" + path.name + "\": {")
+                dict_path = str(f"{directory}/{path.name}")
                 dict_path = dict_path.replace("/", "\"][\"")
-                exec("Assets[\"" + dict_path + "\"] = {}")
+                #exec("Assets[\"" + dict_path + "\"] = {}")
             except Exception as e:
-                pass
-            scan_dir(f"{directory}/{path.name}")
+                print(e)
+            scan_dir(f"{directory}/{path.name}", loops+1)
+            print("  "*loops + "}", end="")
+            #if loops != 0:
+            #    print(",", end="")
+            #   print()
         elif path.is_file():
-            if log:
-                print(f"{directory}/{path.name}")
+            print("  "*loops + "\"" + path.name + "\"", end="")
+            #print(f"{directory}/{path.name}")
             dict_path = str(f"{directory}/{path.name}")
             dict_path = dict_path.replace("/", "\"][\"")
-            exec(f"path[\"{dict_path}\"] = image")
+            #value = path.name
+            #exec(f"Assets[\"{dict_path}\"] = value")
         else:
-            if log:
-                print("IDK what this is")
-                print(path.stat())
+            print("IDK what this is")
+            print(path.stat())
             pass
+        if i != len(help_me):
+            print(",", end="")
+        print()
 
 
-scan_dir("/")
+scan_dir("./")
