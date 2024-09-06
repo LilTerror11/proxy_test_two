08469d53041b0a6c87e1f98d999d7d5ae3a41158
LilTerror11 <144447975+LilTerror11@users.noreply.github.com>
2024-09-06 13:17:04 +0930

    Update scan.py

--- scan.py
+++ scan.py
@@ -40,7 +40,8 @@ def scan_dir(directory: str, loops=0):
             #    print(",", end="")
             #   print()
         elif path.is_file():
-            print("  "*loops + "\"" + path.name + "\"", end="")
+            dat = f.open(path.path, "r").read()
+            print("  "*loops + "\"" + path.name + "\": " + dat, end="")
             #print(f"{directory}/{path.name}")
             dict_path = str(f"{directory}/{path.name}")
             dict_path = dict_path.replace("/", "\"][\"")
